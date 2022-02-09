1b8d98ddca8d27040f910b54ac263904a774b585
Dave Cordero <daverencordero@gmail.com>
2022-02-09 21:59:48 +0800

    webpack optimizations

--- packages/press-x/dist/press-x.js
+++ packages/press-x/dist/press-x.js
@@ -1,1 +1,105 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports["react-change"]=t():e["react-change"]=t()}(self,(function(){return(()=>{"use strict";var e={d:(t,o)=>{for(var r in o)e.o(o,r)&&!e.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:o[r]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};e.r(t),e.d(t,{default:()=>o});var o=function(){};return t})()}));
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["press-x"] = factory();
+	else
+		root["press-x"] = factory();
+})(self, function() {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/press-x.ts":
+/*!************************!*\
+  !*** ./src/press-x.ts ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "test": () => (/* binding */ test)
+/* harmony export */ });
+var test = function test(str) {
+  return str.toLocaleLowerCase();
+};
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* reexport safe */ _press_x__WEBPACK_IMPORTED_MODULE_0__.test)
+/* harmony export */ });
+/* harmony import */ var _press_x__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./press-x */ "./src/press-x.ts");
+
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
+//# sourceMappingURL=press-x.js.map
--- packages/press-x/webpack.config.js
+++ packages/press-x/webpack.config.js
@@ -1,35 +1,42 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'production',
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.[m?j|t]s$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-typescript'
-                        ]
+module.exports = (env) => {
+    const {production: isProduction} = env;
+    const envMode = isProduction ? 'production' : 'development'
+
+    return {
+        mode: envMode,
+        watch: !isProduction,
+        devtool: isProduction ? 'eval-source-map' : 'source-map',
+        entry: './src/index.ts',
+        output: {
+            filename: 'press-x.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+            library: {
+                name: "press-x",
+                type: "umd"
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.[m?j|t]s$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-typescript'
+                            ]
+                        }
                     }
                 }
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.ts'],
-    },
-    output: {
-        filename: 'press-x.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        library: {
-            name: "react-change",
-            type: "umd"
+            ],
         },
-    }
+        resolve: {
+            extensions: ['.ts'],
+        }
+    };
 }
